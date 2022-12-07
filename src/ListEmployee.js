4e5268cc57f87af6dedfa7dad94b871022e5b8fa
Muhammad Ramdhan <n0psledbyte@gmail.com>
2022-12-07 22:45:25 +0700

    add search fitur

--- src/ListEmployee.js
+++ src/ListEmployee.js
@@ -2,21 +2,28 @@ import React, { useState, useEffect, useContext } from "react";
 import "./ListEmployee.css";
 import EmployeeCard from "./components/EmployeeCard";
 import { useNavigate } from "react-router-dom";
-
-
+import Input from "./components/TextInput";
 
 export default function Counter(props) {
   // const [text, setText] = React.useState("");
   // const [showImage] = useState(true);
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
-
 
   const API = "http://103.139.192.200:4000/";
   const ENDPOINT = API + "api/employees";
 
-  const getEmployees = async () => {
-    await fetch(ENDPOINT)
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name + " : " + value);
+    if (name === "search") {
+      setSearch(value);
+    }
+  };
+
+  const getEmployees = async (text) => {
+    await fetch(`${ENDPOINT}?text=${text}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -28,7 +35,7 @@ export default function Counter(props) {
   };
   const handleClick = (employee) => {
     console.log(employee);
-    navigate('/effect',{
+    navigate("/effect", {
       state: {
         id: employee._id,
         name: employee.name,
@@ -41,12 +48,23 @@ export default function Counter(props) {
   };
 
   useEffect(() => {
-    getEmployees();
-  }, []);
+    getEmployees(search);
+  }, [search]);
 
   return (
     <div className="Main">
       <p className="Text">PT Something Employee List</p>
+      <div style={{width: "200px"}}>
+        <Input
+          name="search"
+          label="Search.."
+          locked={false}
+          active={false}
+          onChange={handleInputChange}
+          value={search}
+        />
+
+      </div>
       <div className="container">
         {data.map((employee, idx) => (
           <EmployeeCard
